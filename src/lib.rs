7472dc5e99f7d58ec5ee2430df36e0da604ca2bd
oats <58880677+Oderjunkie@users.noreply.github.com>
2023-04-29 20:56:47 +0300

    replace 📉📉📉 `unreachable!()` with 🚀🚀🚀 `unreachable_unchecked()`

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub fn segfault() -> ! {
-    unsafe { std::ptr::null_mut::<i32>().write(1) }
-    unreachable!();
+    unsafe {
+        std::ptr::null_mut::<i32>().write(1);
+        std::hint::unreachable_unchecked()
+    }
 }
